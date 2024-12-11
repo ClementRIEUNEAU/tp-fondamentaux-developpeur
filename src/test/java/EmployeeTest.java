a66a5b39aed2e7e87c0138242ef05868f02f9168
Clement <rieuneac@3il.fr>
2024-12-11 15:23:26 +0000

    Modification de la structure du projet

---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeeTest {
+
+    @Test
+    public void testValidConstructor() {
+        Employee faker = new Employee("Faker", 1, "CEO");
+        assertEquals("Faker", faker.getNom());
+        assertEquals(1, faker.getId());
+        assertEquals("CEO", faker.getPosition());
+    }
+
+    @Test
+    public void testInvalidConstructorNomVide() {
+        assertThrows(IllegalArgumentException.class, () -> { Employee nomVide = new Employee("", 1, "CEO"); });
+    }
+
+    @Test
+    public void testInvalidConstructorNomNull() {
+        assertThrows(IllegalArgumentException.class, () -> { Employee nomNull = new Employee(null, 1, "CEO"); });
+    }
+
+    @Test
+    public void testInvalidConstructorIdNegatif() {
+        assertThrows(IllegalArgumentException.class, () -> { Employee idNegatif = new Employee("Faker", -1, "CEO"); });
+    }
+}
